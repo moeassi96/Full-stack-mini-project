bf40d0a88701f93100ac0caaec73e33787b3e7c3
moeassi96 <117896769+moeassi96@users.noreply.github.com>
2023-07-20 17:42:06 +0300

    Changed db name

--- src/api/connection.php
+++ src/api/connection.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $db_user = "root";
 $db_pass = null;
-$db_name = "full stack mini project";
+$db_name = "full_stack_mini_db";
 
 $mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
 if ($mysqli->connect_error) { 
